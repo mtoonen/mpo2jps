e81b1ca507ff8129b503d7612cc54dcb201feb4e
HJ200 <>
2012-10-11 10:27:49 +0200

    File cleanup and renaming

--- src/mpo2jps/MPO2JPG.java
+++ src/mpo2jps/MPO2JPG.java
@@ -18,14 +18,14 @@ import java.util.TreeSet;
  *
  * @author gertjan.al
  */
-public class MPOtesterGertjan {
+public class MPO2JPG {
 
     private OutputStream byteOutputStream;
     private SortedSet<Integer> noProblem = new TreeSet<Integer>();
     private SortedSet<Integer> problem = new TreeSet<Integer>();
     private File root = new File("C:/mpooutput");
 
-    public MPOtesterGertjan() {
+    public MPO2JPG() {
         if (root.isDirectory() && root.exists()) {
             deleteFolder(root, false);
         }
@@ -48,8 +48,8 @@ public class MPOtesterGertjan {
     }
 
     public static void main(String... args) throws Exception {
-        MPOtesterGertjan test = new MPOtesterGertjan();
-        File file = new File("IMAG0960.mpo");
+        MPO2JPG test = new MPO2JPG();
+        File file = new File("leftright.mpo");
 
         int failcount = 0;
         int succescount = 0;
@@ -73,18 +73,34 @@ public class MPOtesterGertjan {
     }
 
     public int test(File file, int buffersize) throws Exception {
+        byteOutputStream = null;
         InputStream stream = new FileInputStream(file);
         File outputDir = new File(root, "size_" + buffersize);
         outputDir.mkdirs();
 
         byte[] bytes = new byte[buffersize];
-
+        byte[] headerBridge = new byte[6];
         int length = buffersize;
         int last = 0;
         int bitmapcount = 1;
         while ((length = stream.read(bytes, 0, length)) != -1) {
-
+            headerBridge[3] = bytes[0];
+            headerBridge[4] = bytes[1];
+            headerBridge[5] = bytes[2];
+            int offset;
             int index = 0;
+            if ((offset = isHeader(headerBridge)) != -1) {
+                byteOutputStream.write(headerBridge, 0, offset);
+                addBitmap(byteOutputStream);
+                byteOutputStream = new FileOutputStream(new File(outputDir, "mpo_bitmap" + bitmapcount++ + ".jpg"));
+                byteOutputStream.write(headerBridge, offset, 4);
+                index = offset + 1;
+                last = index;
+            } else {
+                if (byteOutputStream != null) {
+                    byteOutputStream.write(headerBridge, 0, 3);
+                }
+            }
             do {
                 int indexOffset = 0;
                 if (fixNegative(bytes[index]) == 0xff) {
@@ -116,8 +132,12 @@ public class MPOtesterGertjan {
                 index += indexOffset;
             } while (index < (bytes.length - 4));
 
+            headerBridge[0] = bytes[bytes.length - 3];
+            headerBridge[1] = bytes[bytes.length - 2];
+            headerBridge[2] = bytes[bytes.length - 1];
             try {
-                byteOutputStream.write(bytes, last, Math.min(Math.min(index - last + 4, length), length - last));
+                byteOutputStream.write(bytes, last, Math.min(Math.min(index - last + 4 /*offset for number of headerbytes*/ /* offset for overbrugging*/,
+                        length), length - last));
 
             } catch (IndexOutOfBoundsException e) {
                 System.err.println("");
@@ -138,6 +158,21 @@ public class MPOtesterGertjan {
         }
 
         return bitmapcount;
+    }
+
+    private int isHeader(byte[] bytes) {
+        for (int index = 0; index < 3; index++) {
+            if (fixNegative(bytes[index]) == 0xff) {
+                if (fixNegative(bytes[index + 1]) == 0xd8) {
+                    if (fixNegative(bytes[index + 2]) == 0xff) {
+                        if (fixNegative(bytes[index + 3]) == 0xe1) {
+                            return index;
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     private static int fixNegative(byte bytesMeine) {
